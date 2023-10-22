a695ecce8587a87f2dc91e4bbad061757ccfa6e0
Vedant Badre <vedantbadre18@gmail.com>
2023-10-23 02:11:59 +0530

    register function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -10,6 +12,25 @@ class HomePage extends StatelessWidget {
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
+
+  Future<String> registerData() async {
+    final url = Uri.parse(
+        'https://getcode-ndef-api.vercel.app/register_user?email=${user.email}&name=${user.displayName}&uid=${user.uid}&pic_url=${user.photoURL}');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        String message = data['msg'];
+        return message;
+      } else {
+        return "Failed to load data";
+      }
+    } catch (e) {
+      return "Failed to load data: $e";
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +43,7 @@ class HomePage extends StatelessWidget {
       ]),
       body: Center(
           child: Text(
-        "Logged in AS: " + user.email! +" "+user.uid,
+        "Logged in AS: " + user.email! + " " + user.uid,
         style: TextStyle(fontSize: 20),
       )),
     );
